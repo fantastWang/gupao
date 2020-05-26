572017b95f0d5648414fc2aa744e5fbb27d00943
18010913593 <1061505447@qq.com>
2020-05-26 23:29:04 +0800

    添加注释

--- src/main/java/top/wangcj/chapter03分布式与高并发/chapter01了解多线程的意义和使用/demo02/Demo02.java
+++ src/main/java/top/wangcj/chapter03分布式与高并发/chapter01了解多线程的意义和使用/demo02/Demo02.java
@@ -13,7 +13,8 @@ public class Demo02 {
         new Thread(() -> {
             while (true) {
                 try {
-                    TimeUnit.SECONDS.sleep(100);//一种阻塞状态
+                    /**线程会变成一种阻塞状态 TIMED_WAITING：sleep(1)是一种定时器的概念，当时间结束会交给CPU时间片调度执行**/
+                    TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -24,7 +25,8 @@ public class Demo02 {
             while (true) {
                 synchronized (Demo02.class) {
                     try {
-                        Demo02.class.wait();//另一种阻塞状态
+                        /**线程会变成另一种阻塞状态 WAITING：因为是wait()引起的阻塞**/
+                        Demo02.class.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -32,15 +34,16 @@ public class Demo02 {
             }
         }, "Thread02").start();
 
-        //创建两个线程去抢夺资源
-        new Thread(new BlockDemo(), "BlockDemo-01").start();
-        new Thread(new BlockDemo(), "BlockDemo-02").start();
+        //创建两个线程去抢占锁
+        new Thread(new BlockDemo(), "BlockDemo-01").start(); /**首先抢占了线程，因为sleep了所以是TIMED_WAITING**/
+        new Thread(new BlockDemo(), "BlockDemo-02").start(); /**被别的线程一直持有，无法抢占，所以是BLOCKED**/
     }
 
     static class BlockDemo extends Thread {
         @Override
         public void run() {
             synchronized (BlockDemo.class) {
+                /**让抢到线程的线程一直持有这把锁**/
                 while (true) {
                     try {
                         TimeUnit.SECONDS.sleep(100);
