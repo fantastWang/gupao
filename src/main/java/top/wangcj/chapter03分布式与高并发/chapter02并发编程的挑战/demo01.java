6faedf7a65d6d3a53c55679bd03dec42b7763c17
fantast <1061505447@qq.com>
2020-05-30 16:24:43 +0800

    synchronized同步锁

--- src/main/java/top/wangcj/chapter03分布式与高并发/chapter02并发编程的挑战/demo01.java
+++ src/main/java/top/wangcj/chapter03分布式与高并发/chapter02并发编程的挑战/demo01.java
@@ -6,17 +6,21 @@ package top.wangcj.chapter03分布式与高并发.chapter02并发编程的挑战
  * @Date 2020/5/28 20:01
  **/
 public class demo01 {
-    public static int count = 0;
+    public static Integer count = 0;
+
     public static void incr() {
-        count++;
+        System.out.println(Thread.currentThread().getId());
+        synchronized (count) {
+            count++;
+        }
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 1000; i++) {
-            new Thread(() -> {
-                demo01.incr();
-            }).start();
+            new Thread(() -> demo01.incr()).start();
         }
         //结果是小于等于1000的随机数
+        Thread.sleep(2000);
         System.out.println("运行结果" + count);
     }
 }
